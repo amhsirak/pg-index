6d04ad12991fc969d8591af6e9952adfebd0abb8
Karishma <carishmashukla28@gmail.com>
2023-08-07 15:54:24 +0530

    >>> insert 50million rows

---
+++ customers.sql
@@ -0,0 +1,8 @@
+CREATE TABLE customers(first_name VARCHAR(50), last_name VARCHAR(50), mobile_no INT, country VARCHAR(50))
+
+INSERT INTO customers (first_name, last_name, mobile_no, country)
+SELECT substr(md5(random()::text), 1, 10),
+	   substr(md5(random()::text), 1, 10),
+       (random() * 70 + 10)::integer,
+	   (CASE WHEN random() < 0.5 THEN 'India' ELSE 'United Kingdom' END)
+FROM generate_series(1, 50000000);
